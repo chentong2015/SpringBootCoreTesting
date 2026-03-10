21094ec1c0b5f920858368429d8bbc3bcfe1183e
chent5 <chen2015tong@gmail.com>
2026-03-10 17:04:39 +0100

    add context cache module

--- boot-integration-test/src/test/java/spring/SpringBootItConfig.java
+++ boot-integration-test/src/test/java/spring/SpringBootItConfig.java
@@ -1,7 +1,6 @@
 package spring;
 
 import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
@@ -11,9 +10,7 @@ import org.springframework.jmx.export.naming.ObjectNamingStrategy;
 public class SpringBootItConfig {
 
     // TODO. 配置同一次测试中启动两个Spring boot应用
-    // 将启动的App组成一个RuleChain:
-    // - 模拟作为client端内层启动
-    // - 模拟作为Server端启动提供Controller
+    // 将启动App组成RuleChain: 模拟client端内层启动 + Server端启动提供Controller
     @Bean
     ObjectNamingStrategy objectNamingStrategy() {
         ParentAwareNamingStrategy strategy = new ParentAwareNamingStrategy(new AnnotationJmxAttributeSource());
